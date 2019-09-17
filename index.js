a93c688df6b79bc65c892b6ab598cee17ed6edae
jordynniara <jnsmith96@yahoo.com>
2019-09-17 14:14:29 -0500

    Added verse on minute timer

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 require('dotenv').config();
 const director = require('director');
-const Server = require('./lib/Server.js');
+const axios = require('axios');
 
-// create a router for certain paths
-const router = new director.http.Router({
-  '/' : {
-    post: Server.post,
-    get:  Server.ping
-  }
-});
-const server = new Server(router, process.env.PORT);
-server.serve();
+const Run = function()
+{
+    const mannaUrl = 'http://www.ourmanna.com/verses/api/get?format=text&order=random';
+    let verse = '';
+    if((new Date().getMinutes() % 1) == 0 )
+    {
+        // get bible verse
+        axios.get(mannaUrl)
+            .then((response) => {
+                verse = response.data;
+
+                const verseMessage = 
+                    `Good morning everyone!\n`+
+                    `Today's verse of the day: "${verse}"\n`+
+                    `Have a great day! 😄`
+
+                // send it to group me
+                PostMessage(verseMessage);
+            })
+            .catch((err) => console.error('Error getting verse: ' + err))
+    }
+}
+
+const PostMessage = function(verseMessage)
+{
+    const groupMeUrl = `${process.env.BASE_URL}/v3/bots/post`;
+    const data =
+    {
+        "bot_id" : process.env.BOT_ID,
+        "text" : verseMessage,
+    }
+    console.log(`Sending verse, ${verse}, to group ${process.env.GROUP_ID}`)
+    axios.post(url, data)
+        .then(response => {
+            console.log("It is done")
+        })
+        .catch(error => console.error("Error posting message" + error));
+}
+Run();
